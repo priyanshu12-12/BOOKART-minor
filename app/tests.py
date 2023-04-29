3738a87aa44489bc2a8f01ac23fc2d8345efdb1c
Priyanshu Sinha <72189050+priyanshu12-12@users.noreply.github.com>
2023-04-29 12:25:10 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,20 +1,28 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from .models import Product
+from .models import Customer
 
-
-class ShopViewTestCase(TestCase):
+class CustomerTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        self.url = reverse('shop')
-        self.product1 = Product.objects.create(name='Product 1', description='Description for product 1', img='images/product1.jpg')
-        self.product2 = Product.objects.create(name='Product 2', description='Description for product 2', img='images/product2.jpg')
+        self.user = Customer.objects.create(username='testuser', password='testpassword', email='testuser@test.com')
+        
+    def test_customer_login(self):
+        response = self.client.post(reverse('index'), {'username': 'testuser', 'password': 'testpassword'})
+        self.assertEqual(response.status_code, 302) # Check that the response is a redirect
+        self.assertRedirects(response, '/home/') # Check that the response redirects to the correct page
 
-    def test_shop_view(self):
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'shop.html')
-        self.assertContains(response, self.product1.name)
-        self.assertContains(response, self.product1.description)
-        self.assertContains(response, self.product2.name)
-        self.assertContains(response, self.product2.description)
+    def test_customer_registration(self):
+        response = self.client.post(reverse('register'), {'username': 'newuser', 'password': 'newpassword', 'email': 'newuser@test.com'})
+        self.assertEqual(response.status_code, 302) # Check that the response is a redirect
+        self.assertRedirects(response, '/') # Check that the response redirects to the correct page
+
+    def test_shop_page(self):
+        response = self.client.get(reverse('shop'))
+        self.assertEqual(response.status_code, 200) # Check that the response is successful
+        self.assertContains(response, self.user.username) # Check that the response contains the username of the test user
+
+    def test_require_page(self):
+        response = self.client.post(reverse('require'), {'name': 'Test User', 'number': '1234567890', 'bookname': 'Test Book', 'edition': '1st'})
+        self.assertEqual(response.status_code, 200) # Check that the response is successful
+        self.assertTrue(response.context['form'].is_valid()) # Check that the form is valid
