3c6e0e83e6f1b9e5cd249136e16ed31993863068
Priyanshu Sinha <72189050+priyanshu12-12@users.noreply.github.com>
2023-04-29 12:28:02 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,28 +1,39 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from .models import Customer
+from .models import Customer, Product, Requirments
 
-class CustomerTestCase(TestCase):
+
+class TestViews(TestCase):
+
     def setUp(self):
         self.client = Client()
-        self.user = Customer.objects.create(username='testuser', password='testpassword', email='testuser@test.com')
-        
-    def test_customer_login(self):
-        response = self.client.post(reverse('index'), {'username': 'testuser', 'password': 'testpassword'})
-        self.assertEqual(response.status_code, 302) # Check that the response is a redirect
-        self.assertRedirects(response, '/home/') # Check that the response redirects to the correct page
+        self.home_url = reverse('home')
+        self.shop_url = reverse('shop')
+        self.require_url = reverse('require')
+        self.product = Product.objects.create(name='Product 1', description='Product 1 description', img='product1.jpg')
 
-    def test_customer_registration(self):
-        response = self.client.post(reverse('register'), {'username': 'newuser', 'password': 'newpassword', 'email': 'newuser@test.com'})
-        self.assertEqual(response.status_code, 302) # Check that the response is a redirect
-        self.assertRedirects(response, '/') # Check that the response redirects to the correct page
+    def test_home_GET(self):
+        response = self.client.get(self.home_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
 
-    def test_shop_page(self):
-        response = self.client.get(reverse('shop'))
-        self.assertEqual(response.status_code, 200) # Check that the response is successful
-        self.assertContains(response, self.user.username) # Check that the response contains the username of the test user
+    def test_shop_GET(self):
+        response = self.client.get(self.shop_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop.html')
 
-    def test_require_page(self):
-        response = self.client.post(reverse('require'), {'name': 'Test User', 'number': '1234567890', 'bookname': 'Test Book', 'edition': '1st'})
-        self.assertEqual(response.status_code, 200) # Check that the response is successful
-        self.assertTrue(response.context['form'].is_valid()) # Check that the form is valid
+    def test_require_POST(self):
+        response = self.client.post(self.require_url, {
+            'name': 'John Doe',
+            'number': 1234567890,
+            'bookname': 'Book 1',
+            'edition': 'First Edition'
+        })
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(Requirments.objects.filter(name='John Doe', number=1234567890, bookname='Book 1', edition='First Edition').exists())
+
+    def test_purchase_GET(self):
+        response = self.client.get(reverse('purchase', args=[self.product.id]))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'purchase.html')
+        self.assertEquals(response.context['product'], self.product)
