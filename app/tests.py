ef2302120a7dde4451b93d0eb3f0bc0ab694ccab
Priyanshu Sinha <72189050+priyanshu12-12@users.noreply.github.com>
2023-04-29 12:22:13 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Product
 
-# Create your tests here.
+
+class ShopViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('shop')
+        self.product1 = Product.objects.create(name='Product 1', description='Description for product 1', img='images/product1.jpg')
+        self.product2 = Product.objects.create(name='Product 2', description='Description for product 2', img='images/product2.jpg')
+
+    def test_shop_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop.html')
+        self.assertContains(response, self.product1.name)
+        self.assertContains(response, self.product1.description)
+        self.assertContains(response, self.product2.name)
+        self.assertContains(response, self.product2.description)
